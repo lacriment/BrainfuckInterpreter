f2bc3738df9bb1ca6847f82afafc20a1ef5fa22f
Taha İbrahim <hacigesserit@gmail.com>
2015-04-04 00:08:24 +0300

    Initial Commit

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(int argc, const char *argv[])
 {
     if(argc != 2 ){
-        printf("Please use like this: %s filename\n", argv[0]);
+        printf("Please use like this: ./BrainfuckInterpreter filename\n");
     }
     else{
         FILE *fp;
@@ -24,8 +24,8 @@ int main(int argc, const char *argv[])
         tokens[6] = '[';
         tokens[7] = ']';
 
-        int pointer = 0, step = 0,stepback = 0, i = 0;
-        int tape[300];
+        unsigned int pointer = 0, step = 0,stepback = 0, i = 0;
+        int tape[30000];
         //memset(tape, 0, 20*sizeof(int)); //does not work
         for (i = 0; i < sizeof(tape) / 4; i++) {
             tape[i] = 0;
@@ -40,6 +40,7 @@ int main(int argc, const char *argv[])
             }
         }
         char commands[boyut];
+        //char *commands = malloc(boyut);
         fp = fopen(argv[1], "r");
         int addTo = 0;
         while((ch = getc(fp)) != EOF){
@@ -86,7 +87,7 @@ int main(int argc, const char *argv[])
             step++;
         }
             for (i = 0; i < sizeof(tape) / 4; i++) {
-                printf("[%d]", tape[i]);
+                //printf("[%d]", tape[i]);
             }
         fclose(fp);
     }
